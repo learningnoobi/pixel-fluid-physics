6330e443fee2c8a23a3abf212e434e6a36667307
learningnoobi <fanime492@gmail.com>
2022-04-18 18:16:20 +0545

    what

--- js/sand.js
+++ js/sand.js
@@ -1,36 +1,27 @@
 let root = document.documentElement;
-const buttons= document.getElementsByTagName("button")
+const buttons = document.getElementsByTagName("button");
 const mainscene = document.querySelector("#mainscene");
 const wallBtn = document.querySelector("#addWall");
 const sandBtn = document.querySelector("#addSand");
 
-let gridxgrid = 60;
-
+let gridxgrid = 10;
 
 root.style.setProperty("--row-columns", gridxgrid);
 
 let addState = "";
 
-
-
-
-
-
-for (let btn of buttons){
-    btn.addEventListener("click", (e) => {
-       let myState = e.target.name
-        addState = myState
-        btn.textContent = `In ${myState} State`; 
-        for (let i of buttons){
-            if(i.name!==myState){
-                i.textContent=`Add ${i.name}`
-            }
-        }
-      
-    });
+for (let btn of buttons) {
+  btn.addEventListener("click", (e) => {
+    let myState = e.target.name;
+    addState = myState;
+    btn.textContent = `In ${myState} State`;
+    for (let i of buttons) {
+      if (i.name !== myState) {
+        i.textContent = `Add ${i.name}`;
+      }
+    }
+  });
 }
-
-
 
 function createGrid(nxn) {
   for (let i = 0; i < nxn * nxn; i++) {
@@ -40,95 +31,84 @@ function createGrid(nxn) {
     mainscene.appendChild(cell);
 
     cell.addEventListener("mouseover", () => {
-      if (addState.length >0) {
-        cellListener(cell)
+      if (addState.length > 0) {
+        cellListener(cell);
       }
     });
   }
 }
-
+var sandInterval ;
+var sandIdSet = new Set()
+var sandIdArr=[]
 createGrid(gridxgrid);
 
-
 function cellListener(cell) {
-    switch (addState) {
-        case "wall":
-            cell.classList.add("wallIn");
-            cell.setAttribute("data-is-wall", true);
-            break;
-        case "sand":
-            cell.classList.add("sandIn");
-            cell.setAttribute("data-issand", true);
-            const sanddata = new Set([...document.querySelectorAll(`[data-issand="${true}"]`)])
-            moveSand(sanddata)
-            
+  switch (addState) {
+    case "wall":
+      cell.classList.add("wallIn");
+      cell.setAttribute("data-is-wall", true);
+      break;
 
-            break;
-        
-        case "water":
-            cell.classList.add("waterIn");
-            cell.setAttribute("data-is-water", true);
-            break;
-      
-          default:
-              break;
-      }
+    case "sand":
+      cell.classList.add("sandIn");
+      cell.setAttribute("data-issand", true);
+      sandIdArr = [...document.querySelectorAll(`[data-issand="${true}"]`)].map(i=>{
+        return parseInt(i.dataset.cellid)
+      });
+      console.log(sandIdArr)
+     
+      sandInterval = setInterval(moveSandDown, 1000);
+      break;
+
+    case "water":
+      cell.classList.add("waterIn");
+      cell.setAttribute("data-is-water", true);
+      break;
+
+    default:
+      break;
+  }
 }
-
-function moveSand(sanddata) {
-    for(let i of sanddata){
-
-        // setInterval(()=>{
-        //     moveDown(i)
-        // }, 1000);
-    }
-}
-
+mainscene.addEventListener("mouseleave",()=>{
+  if(sandInterval){
+    clearInterval(sandInterval)
+  }
+})
 var currNum = gridxgrid / 2;
 
+function arrayRemove(array, value) {
+  const index = array.indexOf(value);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+}
 
 function draw(id) {
-  const currentCell=document.querySelector(`[data-cellid="${id}"]`)
+  const currentCell = document.querySelector(`[data-cellid="${id}"]`);
   currentCell.classList.add("sandIn");
 }
 
 function unDraw(lastnum) {
-    const currentCell = document.querySelector(`[data-cellid="${lastnum}"]`)
-    if(currentCell){
-        currentCell.classList.remove("sandIn");
-    }
-}
-setInterval(moveDown, 100);
-  //  setInterval(moveown, 10);
-
-var sandDataId=5
-var sandDaaId=10
-
-var sandIdArr=new Array(4,884,1699,7,94,2)
-
-function moveDown(){
-  for (let i=0; i<sandIdArr.length; i++) {
-    if (sandIdArr[i] < Math.pow(gridxgrid, 2)) {
-    draw(sandIdArr[i]);
-    sandIdArr[i] += gridxgrid
-    unDraw(sandIdArr[i]-gridxgrid-gridxgrid)
-    }
+  const currentCell = document.querySelector(`[data-cellid="${lastnum}"]`);
+  if (currentCell) {
+    currentCell.classList.remove("sandIn");
   }
 }
 
-// function moveDown() {
+function moveSandDown() {
   
-//   if (sandDataId < Math.pow(gridxgrid, 2)) {
-//     draw(sandDataId);
-//     sandDataId += gridxgrid;
-//     unDraw(sandDataId-gridxgrid-gridxgrid)
-//   }
-// }
-// function moveown() {
+  for (let i = 0; i < sandIdArr.length; i++) {
+    if (sandIdArr[i] <= Math.pow(gridxgrid, 2)) {
+      draw(sandIdArr[i]);
+      sandIdArr[i] += gridxgrid;
+      unDraw(sandIdArr[i] - gridxgrid - gridxgrid);
+      clearInterval(sandInterval)
+    }
+    else{
+        arrayRemove(sandIdArr, sandIdArr[i]);
+      }
+  }
+
   
-//     if (sandDaaId < Math.pow(gridxgrid, 2)) {
-//       draw(sandDaaId);
-//       sandDaaId += gridxgrid;
-//       unDraw(sandDaaId-gridxgrid-gridxgrid)
-//     }
-//   }
+  console.log(sandIdArr)
+}
